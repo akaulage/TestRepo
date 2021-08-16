50c70336a33c222d3291a3f3d3182b34b4d48216
akaulage <kaulageavi@gmail.com>
2021-08-16 15:06:40 +0530

    Added Hystrix-dashboard module

--- Hystrix-DashBoard/src/main/java/com/akaulage/hystrixdashboard/HystrixDashBoardApplication.java
+++ Hystrix-DashBoard/src/main/java/com/akaulage/hystrixdashboard/HystrixDashBoardApplication.java
@@ -1,17 +1,17 @@
-package com.example.apigateway;
+package com.akaulage.hystrixdashboard;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 @SpringBootApplication
+@EnableHystrixDashboard
 @EnableEurekaClient
-@EnableHystrix
-public class ApiGatewayApplication {
+public class HystrixDashBoardApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ApiGatewayApplication.class, args);
+        SpringApplication.run(HystrixDashBoardApplication.class, args);
     }
 
 }
